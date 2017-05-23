e5b70cf365023357264f65de6ed4ba72b5987c30
Václav Klusák <vaclav.klusak@klokantech.com>
2017-05-23 17:15:14 +0200

    Release 1.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Cron',
-    version='1.0',
+    version='1.1',
     description='Periodic task execution for Flask',
     py_modules=['flask_cron'],
     install_requires=[
